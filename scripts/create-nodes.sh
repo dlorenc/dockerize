2ae6cb2ecf646b9487254b2913140ded994b74ef
Dan Lorenc <lorenc.d@gmail.com>
2015-01-01 15:23:08 -0800

    Ansible.

--- scripts/create-nodes.sh
+++ scripts/create-nodes.sh
@@ -1,3 +1,6 @@
 for node in $(seq 0 2); do
 	gcloud compute instances create node-$node --zone=us-central1-f --scopes=storage-full --image=container-vm-v20141208 --image-project=google-containers --tags=nodes
 done
+gcloud compute firewall-rules create nodes --allow tcp:8080 --target-tags nodes
+gcloud compute target-pools add-instances webservers --zone=us-central1-f --instances node-0 node-1 node-2
+gcloud compute forwarding-rules create webserversrule --target-pool webservers --region=us-central1
