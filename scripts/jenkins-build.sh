de3c7e09c2ffa7a354e90c398507a6693da70139
Dan Lorenc <lorenc.d@gmail.com>
2015-01-01 21:39:32 -0800

    Fast deployments.

--- scripts/jenkins-build.sh
+++ scripts/jenkins-build.sh
@@ -5,7 +5,8 @@ IMAGE=dockerize_$BUILD_NUMBER
 # Build the Docker image
 docker build -t $IMAGE .
 # Save it to a tar.gz
-docker save -o $IMAGE.tar $IMAGE
-gzip $IMAGE.tar
+docker save $IMAGE > gzip > $IMAGE.tar.gz
+docker rmi $IMAGE
 # Upload it to GCS
 gsutil cp $IMAGE.tar.gz gs://dockerize-builds/
+rm $IMAGE.tar.gz
